ef2faa9b43d33be6ef93525c002ddea73de927f0
Epiiko <nachodiazromerodev@gmail.com>
2025-10-09 15:57:25 +0200

    fix: cambio login

---
+++ utils/personajes.js
@@ -0,0 +1,32 @@
+import { USUARIOS } from "./equipament.js"
+function checkLocalStorage() {
+    const nickEscrito = document.querySelector("#username").value;
+    const passEscrita = document.querySelector("#password").value;
+    if (nickEscrito && passEscrita) {
+        checkUser(nickEscrito, passEscrita)
+    } else {
+        if (!document.querySelector(".error-msg")) {
+            document
+                .querySelector(".envioLogin")
+                .insertAdjacentHTML("beforebegin", '<p class="error-msg">Error en el usuario y/o contraseña</p>');
+        }
+
+    }
+}
+function checkUser(nickEscrito, passEscrita) {
+    USUARIOS.forEach(user => {
+        if (user.name == nickEscrito && btoa(passEscrita) == user.password) {
+            document.querySelector(".login").style.display = "none";
+            return;
+        } else {
+            if (!document.querySelector(".error-msg")) {
+                document
+                    .querySelector(".envioLogin")
+                    .insertAdjacentHTML("beforebegin", '<p class="error-msg">Error en el usuario y/o contraseña</p>');
+            }
+        }
+    });
+}
+document.querySelector(".envioLogin").addEventListener("click", () => {
+    checkLocalStorage()
+})
